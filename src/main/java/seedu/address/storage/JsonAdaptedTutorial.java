d89ea2c84b7f2d0c0bf126896b323a16ed29f5a1
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-10-19 23:21:49 +0800

    Add Tutorial saving and loading json Eduvault does not support Tutorial saving.
    Tutorials will be integral in the application.
    
    Let's add json Tutorial support.

--- src/main/java/seedu/address/storage/JsonAdaptedTutorial.java
+++ src/main/java/seedu/address/storage/JsonAdaptedTutorial.java
@@ -3,25 +3,38 @@ package seedu.address.storage;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.tutorial.Tutorial;
+
+
 /**
  * Jackson-friendly version of {@link Tutorial}.
  */
 public class JsonAdaptedTutorial {
-    private final String name;
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Tutorial's %s field is missing!";
+    private final String subject;
     /**
      * Constructs a {@code JsonAdaptedTutorial} with the given tutorial details.
      */
     @JsonCreator
-    public JsonAdaptedTutorial(@JsonProperty("name") String name) {
-        this.name = name;
+    public JsonAdaptedTutorial(@JsonProperty("subject") String subject) {
+        this.subject = subject;
     }
-    //    TODO
-    //    public Tutorial toModelType() throws IllegalValueException {
-    //        return new Tutorial(this.name);
-    //    }
-
-    @Override
-    public String toString() {
-        return this.name;
+    /**
+     * Converts a given {@code Tutorial} into this class for Jackson use.
+     */
+    public JsonAdaptedTutorial(Tutorial source) {
+        subject = source.getSubject();
+    }
+    /**
+     * Converts this Jackson-friendly adapted tutorial object into the model's {@code Tutorial} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted tutorial.
+     */
+    public Tutorial toModelType() throws IllegalValueException {
+        if (subject == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, String.class.getSimpleName()));
+        }
+        return new Tutorial(this.subject);
     }
 }
